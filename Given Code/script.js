309bb997d5c54088c99e1945bc1c16d2107f082a
Damion Morgan <only4school5680@gmail.com>
2023-08-03 22:08:10 -0400

    about to create new branch

--- Given Code/script.js
+++ Given Code/script.js
@@ -1,14 +1,33 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// //const generatePassword=()=>{
 
-  passwordText.value = password;
+// //}
 
-}
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   let passwordText = document.querySelector("#password");
+//   passwordText.value = password;
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+
+
+const passwordOptions={
+  alphabetUpper : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  specialCharacters : ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', ';', ':', ',', '.', '/', '<', '>', '?'],
+  numbersArray : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    }
+    for(const passwordArrays in passwordOptions){
+    // console.log(`${passwordArrays}:${passwordOptions[passwordArrays]}`)
+     const generatePassword=passwordOptions[passwordArrays].toString()
+     const randomIndex =Math.floor(Math.random()*generatePassword.length)
+      const randomCharacter=generatePassword[randomIndex]
+      console.log(randomCharacter)
+    };
