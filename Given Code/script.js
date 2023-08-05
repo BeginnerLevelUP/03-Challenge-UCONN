49099f6e5c55d2f0e8a649c758782ce874d26a62
Damion Morgan <only4school5680@gmail.com>
2023-08-05 12:20:19 -0400

    instead of removing from the array why not just add to it

--- Given Code/script.js
+++ Given Code/script.js
@@ -1,6 +1,3 @@
-const testing=document.getElementById('testing');
-const testing2=document.getElementById('testing2');
-
 // All the characters needed to make a password placed inside an object
 const passwordObject={
  alphabetUpper : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
@@ -11,7 +8,7 @@ specialCharacters : ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',
 
 };
 
-
+const generatePassword=(length,remove)=>{
 //ALL Characters placed into an array
 let passwordCharacter = [...passwordObject.alphabetUpper,...passwordObject.alphabeLower, ...passwordObject.specialCharacters, ...passwordObject.numbersArray,...passwordObject.alphabetUpper,...passwordObject.alphabeLower, ...passwordObject.specialCharacters, ...passwordObject.numbersArray];
 
@@ -35,11 +32,11 @@ const removeElementsFromArray = (array, elements) => {
   });
 };
 
-removeElementsFromArray(passwordCharacter, passwordObject.alphabeLower);
-removeElementsFromArray(passwordCharacter, passwordObject.alphabeLower);
+removeElementsFromArray(passwordCharacter, remove);
+removeElementsFromArray(passwordCharacter, remove);
 
 // Ammount
-const ammount=128;
+const ammount=length;
 let passwordRandomize = randomizePassword([...passwordCharacter]);
 if (ammount >= passwordCharacter.length) {
 
@@ -49,8 +46,50 @@ if (ammount >= passwordCharacter.length) {
   passwordRandomize = passwordRandomize.slice(0, ammount);
 }
 
-testing.innerHTML = passwordRandomize.join('');
-testing2.innerHTML = passwordCharacter//passwordRandomize.length;
+return passwordRandomize.join('')
+}
+
+//Show the value of the range
+const rangeInput=document.querySelector('#rangeInput')
+rangeInput.value=0; //(Default Value at 0)
+const rangeValue=document.querySelector('#rangeValue')
+
+rangeInput.addEventListener('input',()=>{
+  const value=rangeInput.value
+  rangeValue.textContent=value;
+})
+
+
+// CheckBox Specifications
+const upCase=document.querySelector('upCase')
+upCase.addEventListener('change',()=>{
+
+})
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+const specifyBtn=document.querySelector('#specify')
+// Write password to the #password input
+function writePassword() {
+
+  var password = generatePassword(rangeValue.textContent,'');
+  console.log(password)
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+specifyBtn.addEventListener('click',()=>{
+  const prompt=document.querySelector('.prompt')
+  prompt.style.display='block'
+})
+generateBtn.addEventListener("click", writePassword);
+
 
 
 
